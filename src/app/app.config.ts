664251bc6c97b538594641a33d7e2e4e8ed8a6a1
Théophane <girard.theophane@gmail.com>
2026-03-29 17:46:56 +0200

    set up Firebase Hosting with CI/CD pipeline, integrate Firebase configuration, and initialize Firebase Analytics

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,10 @@
-import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, inject, PLATFORM_ID, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 import { ThemeService } from '@/shared/services/theme.service';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -9,6 +12,7 @@ import { provideZard } from '@/shared/core/provider/providezard';
 import { provideChartTheme } from '@/shared/components/chart/chart-theme';
 import { provideAgGridTheme } from '@/shared/components/table/table-theme';
 import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
+import { firebaseConfig } from './firebase.config';
 
 ModuleRegistry.registerModules([AllCommunityModule]);
 
@@ -17,6 +21,13 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideAppInitializer(() => {
       inject(ThemeService).init();
+    }),
+    provideAppInitializer(() => {
+      const platformId = inject(PLATFORM_ID);
+      const app = initializeApp(firebaseConfig);
+      if (isPlatformBrowser(platformId)) {
+        getAnalytics(app);
+      }
     }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
---
+++ src/app/firebase.config.ts
@@ -0,0 +1,11 @@
+import type { FirebaseOptions } from 'firebase/app';
+
+export const firebaseConfig: FirebaseOptions = {
+  apiKey: 'AIzaSyDo70oSBmq_RzwK0uc9b3pweDxSKyPfPpY',
+  authDomain: 'lol-build-8b2b4.firebaseapp.com',
+  projectId: 'lol-build-8b2b4',
+  storageBucket: 'lol-build-8b2b4.firebasestorage.app',
+  messagingSenderId: '833464166119',
+  appId: '1:833464166119:web:010f7f53c57b9628576442',
+  measurementId: 'G-Z3K2B960J4',
+};
